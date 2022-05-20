a1ff0bdf3842fae3281379c07fcabd54fdcb34e3
abe <zeamanualfeleke@gmail.com>
2022-05-20 14:21:44 +0300

    new features for the cloth api implemented
    feature of creating, deleting and getting clothes by their id is implemnted

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,7 @@ let connect = async(app)=>{
         await mongoose.connect(process.env.DB_CONNECTION_STRING)
         app.listen(process.env.PORT,console.log(`Server is listing on port ${process.env.PORT}`))
     } catch (error) {
-        console.log(error)
-        
-    }
-    
+        console.log(error)   
+    } 
 }
 module.exports=connect
--- src/controllers/cloth.js
+++ src/controllers/cloth.js
@@ -1,21 +1,71 @@
+const CustomError = require('../helpers/customError')
 let ClothModel = require('../models/cloth')
 let OrderModel = require('../models/order')
 let UserModel = require("../models/user")
 
-let getAll = (req,res)=>{
-    
+let getAll = async (req,res,next)=>{
+    try {
+         let clothes= await ClothModel.find({})
+         if(clothes.length<1){
+             throw new CustomError("No cloth found",404)
+         }
+         res.status(200).json({clothes})
+    } catch (error) {
+        next(error)
+    }   
 }
-let getOne = (req,res)=>{
+let getOne =async (req,res,next)=>{
+    let id = req.params?.id
+    try {
+        let cloth = await ClothModel.findOne({name:id})
+        if(cloth){
+            res.status(200).json({cloth})
+        }else{
+            throw new CustomError(`No Cloth With name ${id}`,404)
+        }
+      
+    } catch (error) {
+        next(error)
+    }
     
 }
 let update = (req,res)=>{
     
 }
-let create = (req,res)=>{
+let create = async (req,res,next)=>{
+    try {
+        if(req.body.name && req.body.price){
+
+            let existing = await ClothModel.find({name:req.body?.name})
+            if(existing.length>0){
+                throw new CustomError("Cloth Name Already Exists",400)
+            }
+            let created =  ClothModel({name:req.body.name,price:req.body.price})
+            let response = await created.save()
+            res.status(201).json(response)
+        }
+        else{
+            throw new CustomError("Invalid Input",400)
+        }
+       
+    } catch (error) {
+        next(error)
+    }
     
 }
-let deleteOne = (req,res)=>{
-    
+let deleteOne = async(req,res,next)=>{
+    try {
+        let existing = await ClothModel.findOne({name:req.params.id})
+        if(existing){
+            await ClothModel.findByIdAndDelete(existing._id)
+           return res.status(200).json({msg:"Operation Successful"})
+        }
+        else{
+            throw new CustomError(`Cloth name ${req.params.id} not found`,404)
+        }
+    } catch (error) {
+        next(error)
+    }
 }
 module.exports={
     getAll,
